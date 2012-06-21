dc034dc4e235e205c929874f94569df1cc1794c3
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2012-06-21 10:21:20 +0200

    Define a graphic_state class to save GL states. Refs 1796.

--- point_cloud_factory.cpp
+++ point_cloud_factory.cpp
@@ -42,7 +42,7 @@ PointCloudFactory::PointCloudFactory(const Tao::ModuleApi *tao)
         sdebug() << "VBO supported: " << vboSupported << "\n";
     if (!licenseTested)
     {
-        licensed = tao->checkImpressOrLicense("PointCloud 1.002");
+        licensed = tao->checkImpressOrLicense("PointCloud 1.0");
         licenseTested = true;
     }
 }
