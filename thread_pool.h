d2f4a3e9e19d9861cd35d416722ffb9cd6afde2a
Christophe de Dinechin <christophe@dinechin.org>
2019-03-22 13:07:11 +0100

    Automatic copyright header adjustment

--- thread_pool.h
+++ thread_pool.h
@@ -1,10 +1,10 @@
 #ifndef THREAD_POOL_H
 #define THREAD_POOL_H
-// ****************************************************************************
-//  thread_pool.h                                                  Tao project
-// ****************************************************************************
+// *****************************************************************************
+// thread_pool.h                                                   Tao3D project
+// *****************************************************************************
 //
-//   File Description:
+// File description:
 //
 //    Execute tasks in separate thread(s).
 //
@@ -15,12 +15,27 @@
 //
 //
 //
-// ****************************************************************************
-// This software is licensed under the GNU General Public License v3.
-// See file COPYING for details.
-//  (C) 2012 Jerome Forissier <jerome@taodyne.com>
-//  (C) 2012 Taodyne SAS
-// ****************************************************************************
+// *****************************************************************************
+// This software is licensed under the GNU General Public License v3
+// (C) 2014-2015,2019, Christophe de Dinechin <christophe@dinechin.org>
+// (C) 2012, Jérôme Forissier <jerome@taodyne.com>
+// *****************************************************************************
+// This file is part of Tao3D
+//
+// Tao3D is free software: you can r redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Tao3D is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Tao3D, in a file named COPYING.
+// If not, see <https://www.gnu.org/licenses/>.
+// *****************************************************************************
 
 #include <QList>
 #include <QMutex>
